1e4292390f2a9a02d8e1126bcb3f17118d61621a
Yarden Zeron <yardenzeron@mail.tau.ac.il>
2023-03-07 20:38:41 +0200

    responsive header

--- src/components/app-header.jsx
+++ src/components/app-header.jsx
@@ -1,46 +1,42 @@
+import { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export function AppHeader() {
+    const [menuBar, setMenubar]= useState(false)
 
-    let activeStyle = {
-        textDecoration: "underline",
-    };
-
-    let activeClassName = "underline"
-
+    console.log('menuBar:',menuBar )
     return (
-        <header className="main-layout app-header">
-            <div className="logo">
-                <span></span>
-            </div>
-
-            <main>
-                <span className="decoration-line">
-                    <div className="line"></div>
+        <header className="app-header">
+            <section className="main-layout ">
+                <main>
+                    <div className="logo">
+                        <img src='assets/shared/logo.svg' alt="logo" />
+                    </div>
+                    <span className="decoration-line">
+                        <div className="line"></div>
+                    </span>
+                    <nav className={menuBar? 'active': null}>
+                        <button className="exit-menu" onClick={()=>setMenubar(!menuBar)}>X </button>
+                        <NavLink to="/" className=''>
+                            <span className="pageNumber">00</span> Home
+                        </NavLink>
+                        <NavLink to="destination" className=''>
+                            <span className="pageNumber">01</span> destination
+                        </NavLink>
+                        <NavLink to="crow" className=''>
+                            <span className="pageNumber">02</span> crow
+                        </NavLink>
+                        <NavLink to="technology" className=''>
+                            <span className="pageNumber">03</span> technology
+                        </NavLink>
+                    </nav>
+                    <button className="menu-toggle-btn" onClick={()=>setMenubar(!menuBar)}>
+                        <img src="assets/shared/icon-hamburger.svg" alt="open-menu" />
+                    </button>
+                </main>
+                <span className="linkBox">
                 </span>
-
-                <nav>
-                    <ul >
-                        <li >
-                            <NavLink to="/" className=''>
-                                <span className="pageNumber">00</span> Home
-                            </NavLink>
-                            <NavLink to="destination" className=''>
-                                <span className="pageNumber">01</span> destination
-                            </NavLink>
-                            <NavLink to="crow" className=''>
-                                <span className="pageNumber">02</span> crow
-                            </NavLink>
-                            <NavLink to="technology" className=''>
-                                <span className="pageNumber">03</span> technology
-                            </NavLink>
-                        </li>
-                    </ul>
-                </nav>
-            </main>
-            <span className="linkBox">
-
-            </span>
+            </section>
         </header>
     )
 
