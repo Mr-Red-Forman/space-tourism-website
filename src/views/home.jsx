cb4e15e7bf0ee6874d37517f1e308204ffb91a9c
Yarden Zeron <yardenzeron@mail.tau.ac.il>
2023-03-12 10:46:40 +0200

    home from desktop

--- src/views/home.jsx
+++ src/views/home.jsx
@@ -1,7 +1,18 @@
 export function HomePage() {
     return (
         <section className="main-layout home-page">
-           
+            <main>
+                <article className="welcomeText">
+                    <h3>So, you want to travel to</h3>
+                    <h1>space</h1>
+                    <p>Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!</p>
+                </article>
+                <article className="welcomeBtn">
+                        <h3>explore</h3>
+                </article>
+
+            </main>
+
 
         </section>
     )
